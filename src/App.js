ef4523adf8b3607aff7898100615ec42a668626a
Aman <aman959924@gmail.com>
2022-11-13 16:52:31 +0530

    React-Redux Project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import  {Header}  from './components/Header';
+import { CardDetails } from './components/CardDetails';
+import { Cards } from './components/Cards';
+
+export default function App() {
+  return (
+    
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Cards/>} />
+      <Route path='/cart' element={<CardDetails/>} />
+    </Routes>
+    </BrowserRouter>
+  
+  );
+}
+
+ 
